e5aed6ec7c5da548cb34328d632de3ed945afe2d
Eddy <git@eddydavies.com>
2020-11-08 21:32:10 +0000

    Code works with other example

--- MyAddressValidator.py
+++ MyAddressValidator.py
@@ -3,9 +3,6 @@ import json
 from jsonschema import validate
 
 
-
-
-
 def validate_address(address_file_path):
 #     """This function validates the input JSON ﬁle against your 
 #        schema and returns 
@@ -23,20 +20,25 @@ def validate_address(address_file_path):
 
      x = address_file_path.rfind('/') 
      schema_file_path = address_file_path[:x+1] + "address.json.schema"
-    
-     with open(schema_file_path, "r") as schema:
-          with open(address_file_path, "r") as expr:
 
-               jsonData = json.load(expr)
-               jsonSchema= json.load(schema)
-               print(json.dumps(jsonData, indent=4))
+     print(schema_file_path)
+     schema = open(schema_file_path, "r").read()
+     print(address_file_path)
+     expr = open(address_file_path, "r").read()
 
-               try:
-                    validate(instance=jsonData, schema=jsonSchema)
-               except jsonschema.exceptions.ValidationError as err:
-                    print("Given JSON data is InValid")
-               else:
-                    print("Given JSON data is Valid")
+     jsonSchema = json.loads(schema)
+     jsonData = json.loads(expr)
+
+     print(jsonSchema)
+     print(jsonData)
+
+     try:
+          validate(instance=jsonData, schema=jsonSchema)
+     except jsonschema.exceptions.ValidationError as err:
+          print("Given JSON data is InValid")
+          return "invalid"
+     else:
+          print("Given JSON data is Valid")
 
      pass
 
@@ -45,6 +47,7 @@ if __name__=='__main__':
 #     print(validate_address(address_file_path))
 
      files = [
+     # "/Users/edwarddavies/Documents/UoM/_Modelling Data/CW2/Test.json"
     "/Users/edwarddavies/Documents/UoM/_Modelling Data/M2/ValidNoLarge.json",
 #     "/Users/edwarddavies/Documents/UoM/_Modelling Data/M2/ValidSomeLarge.json",
 #     "/Users/edwarddavies/Documents/UoM/_Modelling Data/M2/Invalid3.json"
